346bdc16b1c24593ad372e661aa666527a9b5c4c
zivul <analiz-k@mail.ru>
2024-07-04 17:03:24 +0300

    Обновление

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from time import sleep
-import logging.config
-from os import path
 import logging
 
 logging.basicConfig(level=logging.DEBUG, filename='../my_log.log',
@@ -15,13 +13,7 @@ handler = logging.FileHandler('../test.log', encoding='utf-8')
 formatter = logging.Formatter('%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-
 logger.info('Тестируем файл на данные')
-
-
-# log_file_level = path.join(path.dirname(path.abspath(__file__)), 'loggin.ini')
-# logging.config.fileConfig(log_file_level)
-
 
 @pytest.fixture()
 def set_up_browser():
--- tests/test_xxx.py
+++ tests/test_xxx.py
@@ -4,10 +4,10 @@ from selenium.webdriver import Keys
 from time import sleep
 import allure
 
-@allure.feature('Оглавление')
-@allure.story('История')
+@allure.feature('Тестирование web-приложений')
+@allure.story('Объекты тестирования')
 class TestExemple():
-    @allure.title('Проверка загрузки страницы')
+    @allure.title('Очистка поля поиска и ввод текста на github')
     def test_search(self, set_up_browser):
         driver = set_up_browser
         with allure.step('Открытие страницы'):
@@ -19,47 +19,67 @@ class TestExemple():
         with allure.step('Ввод текста и Enter'):
             driver.find_element(By.ID, 'query-builder-test').send_keys('bug' + Keys.ENTER)
 
+    @allure.title('Ввод текста в поле и поиск на github')
     def test_filter(self, set_up_browser):
         driver = set_up_browser
-        driver.get('https://github.com/microsoft/vscode/issues')
-        driver.find_element(By.XPATH, "(//span[@class='dropdown-caret hide-sm'])[1]").click()
-        el = driver.find_element(By.ID, 'author-filter-field')
-        text = 'bpasero'
-        for i in text:
-            el.send_keys(i)
-            sleep(0.5)
-        driver.find_element(By.CLASS_NAME, 'js-new-item-name').click()
+        with allure.step('Открытие страницы'):
+            driver.get('https://github.com/microsoft/vscode/issues')
+        with allure.step('Клик в поле'):
+            driver.find_element(By.XPATH, "(//span[@class='dropdown-caret hide-sm'])[1]").click()
+        with allure.step('Ввод текста'):
+            el = driver.find_element(By.ID, 'author-filter-field')
+            text = 'bpasero'
+            for i in text:
+                el.send_keys(i)
+                sleep(0.5)
+        with allure.step('Клик на кнопку'):
+            driver.find_element(By.CLASS_NAME, 'js-new-item-name').click()
 
+    @allure.title('Заполнение формы на github')
     def test_language(self, set_up_browser):
         driver = set_up_browser
-        driver.get('https://github.com/search/advanced')
-        driver.find_element(By.ID, 'search_language').click()
-        driver.find_element(By.XPATH, "(//*[contains(text(), 'Python')])[1]").click()
+        with allure.step('Открытие страницы'):
+            driver.get('https://github.com/search/advanced')
+        with allure.step('Открытие поиска и клик'):
+            driver.find_element(By.ID, 'search_language').click()
+        with allure.step('Клик по кнопке'):
+            driver.find_element(By.XPATH, "(//*[contains(text(), 'Python')])[1]").click()
         sleep(2)
-        driver.find_element(By.ID, 'search_stars').send_keys('>20000')
+        with allure.step('Ввод текста: <20000'):
+            driver.find_element(By.ID, 'search_stars').send_keys('>20000')
         sleep(2)
-        driver.find_element(By.ID, 'search_filename').send_keys('environment.yml')
+        with allure.step('Ввод текста: environment.yml '):
+            driver.find_element(By.ID, 'search_filename').send_keys('environment.yml')
         sleep(2)
-        driver.find_element(By.XPATH, "(//button[@class='btn flex-auto'])[2]").click()
+        with allure.step('Клик по кнопке'):
+            driver.find_element(By.XPATH, "(//button[@class='btn flex-auto'])[2]").click()
 
+    @allure.title('Проверка фильтра на skillbox')
     def test_skillbox(self, set_up_browser):
         driver = set_up_browser
-        driver.get('https://skillbox.ru/code/')
-        driver.find_element(By.XPATH, "(//*[contains(text(), 'Профессия')])[3]").click()
-        action_chains = webdriver.ActionChains(driver)
-        left_button_slider = driver.find_element(By.XPATH, "//div[@aria-valuetext='1']")
-        right_button_slider = driver.find_element(By.XPATH, "//div[@aria-valuetext='24']")
-        action_chains.click_and_hold(left_button_slider).move_by_offset(16, 0).perform()
-        action_chains.click_and_hold(right_button_slider).move_by_offset(-34, 0).pause(1).perform()
-        driver.find_element(By.XPATH, "//span[@class='ui-checkbox-field__value ui-checkbox-field__value--small']")\
+        with allure.step('Открытие формы'):
+            driver.get('https://skillbox.ru/code/')
+        with allure.step('Check-box выбор позиции: Профессия'):
+            driver.find_element(By.XPATH, "(//*[contains(text(), 'Профессия')])[3]").click()
+        with allure.step('Передвижение ползунков'):
+            action_chains = webdriver.ActionChains(driver)
+            left_button_slider = driver.find_element(By.XPATH, "//div[@aria-valuetext='1']")
+            right_button_slider = driver.find_element(By.XPATH, "//div[@aria-valuetext='24']")
+            action_chains.click_and_hold(left_button_slider).move_by_offset(16, 0).perform()
+            action_chains.click_and_hold(right_button_slider).move_by_offset(-34, 0).pause(1).perform()
+        with allure.step('Check-box выбор позиции: 1С'):
+            driver.find_element(By.XPATH, "//span[@class='ui-checkbox-field__value ui-checkbox-field__value--small']")\
         .click()
 
+    @allure.title('Наведение на график мышкой на github')
     def test_week(self, set_up_browser):
         driver = set_up_browser
-        driver.get('https://github.com/microsoft/vscode/graphs/commit-activity')
+        with allure.step('Открытие страницы github'):
+            driver.get('https://github.com/microsoft/vscode/graphs/commit-activity')
         sleep(5)
-        el = driver.find_element(By.XPATH, "(//*[@class='bar mini'])[22]")
-        action = webdriver.ActionChains(driver)
-        action.move_to_element(el).pause(1).perform()
+        with allure.step('Наведение на элемент графика'):
+            el = driver.find_element(By.XPATH, "(//*[@class='bar mini'])[22]")
+            action = webdriver.ActionChains(driver)
+            action.move_to_element(el).pause(1).perform()
 
 
